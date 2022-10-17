251d4c07cbc341054479936ebac2d2a74c304f53
eoinkelly91 <eoindpkelly@gmail.com>
2022-10-17 15:34:10 +0100

    Scarfy initial commit

--- game.cpp
+++ game.cpp
@@ -3,13 +3,25 @@
 int main(){
     const int windowWidth{500};
     const int windowHeight{500};
+    
     InitWindow(windowWidth,windowHeight,"Eoin");
+    Texture2D scarfy =  LoadTexture("resources/scarfy.png");
+    Rectangle scarfyRec;
+    scarfyRec.width = scarfy.width/6;
+    scarfyRec.height = scarfy.height;
+    scarfyRec.x = 0;
+    scarfyRec.y = 0; 
+    Vector2 scarfyPos;
+    scarfyPos.x = windowWidth/2 - scarfyRec.width/2;
+    scarfyPos.y = windowHeight - scarfyRec.height;
+
     SetTargetFPS(60);
     while(WindowShouldClose() == false){
         BeginDrawing();
-        DrawCircle(250,250,10,RED);
-        ClearBackground(BLUE);
+        DrawTextureRec(scarfy,scarfyRec, scarfyPos,WHITE);
+        ClearBackground(WHITE);
         EndDrawing();
     }
+    UnloadTexture(scarfy);
     CloseWindow();
 }
