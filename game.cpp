f283ee8e6a68a7eddf2462d50e01577282484f12
eoinkelly91 <eoindpkelly@gmail.com>
2022-10-27 10:24:33 +0100

    added background

--- game.cpp
+++ game.cpp
@@ -1,15 +1,39 @@
+// import raylib library
 #include "raylib.h"
 
-int main(){
-    const int windowWidth{500};
-    const int windowHeight{500};
-    InitWindow(windowWidth,windowHeight,"Eoin");
+// program main entry point
+int main()
+{
+    // window variables
+    const int windowWidth{1800};
+    const int windowHeight{1200};
+
+    InitWindow(windowWidth, windowHeight, "Eoin"); // Show the game window with title
+
+    Texture2D background = LoadTexture("resources/forest-background.png"); // background variable
+    // charachter variables
+    Texture2D scarfy = LoadTexture("resources/scarfy.png");
+    Rectangle scarfyRec;
+    scarfyRec.width = scarfy.width / 6;
+    scarfyRec.height = scarfy.height;
+    scarfyRec.x = 0;
+    scarfyRec.y = 0;
+    Vector2 scarfyPos;
+    scarfyPos.x = windowWidth / 2 - scarfyRec.width / 2;
+    scarfyPos.y = windowHeight - scarfyRec.height;
+
     SetTargetFPS(60);
-    while(WindowShouldClose() == false){
+    // loop where game logic takes place, only ends on window close
+    while (WindowShouldClose() == false)
+    {
         BeginDrawing();
-        DrawCircle(250,250,10,RED);
-        ClearBackground(BLUE);
+        ClearBackground(WHITE);
+        DrawTexture(background, 0, 0, WHITE);                // add background image
+        DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE); // add charachter image
         EndDrawing();
     }
+    UnloadTexture(background); // unload texture from memory
+    UnloadTexture(scarfy);
     CloseWindow();
+    return 0;
 }
