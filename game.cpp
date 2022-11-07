b8997ecf61eafa69f9c20ce2282b0c4aa58c4240
eoinkelly91 <eoindpkelly@gmail.com>
2022-11-07 20:36:17 +0000

    added object and movement"

--- game.cpp
+++ game.cpp
@@ -1,39 +1,156 @@
-// import raylib library
-#include "raylib.h"
+#include "raylib.h" // import raylib library
+
+struct Anim
+{
+    Rectangle rec;
+    Vector2 pos;
+    int frame;
+    float updateTime;
+    float runningTime;
+};
 
 // program main entry point
 int main()
 {
-    // window variables
-    const int windowWidth{1800};
-    const int windowHeight{1200};
+    const int windowWidth{1800};  // variable to set window width
+    const int windowHeight{1200}; // variable to set window height
+    const int gravity{1000};      // gravity varaible
 
     InitWindow(windowWidth, windowHeight, "Eoin"); // Show the game window with title
 
     Texture2D background = LoadTexture("resources/forest-background.png"); // background variable
-    // charachter variables
+
+    // character variables
     Texture2D scarfy = LoadTexture("resources/scarfy.png");
-    Rectangle scarfyRec;
-    scarfyRec.width = scarfy.width / 6;
-    scarfyRec.height = scarfy.height;
-    scarfyRec.x = 0;
-    scarfyRec.y = 0;
-    Vector2 scarfyPos;
-    scarfyPos.x = windowWidth / 2 - scarfyRec.width / 2;
-    scarfyPos.y = windowHeight - scarfyRec.height;
+    Anim scarfyAnim;
+    scarfyAnim.rec.width = scarfy.width / 6;
+    scarfyAnim.rec.height = scarfy.height;
+    scarfyAnim.rec.x = 0;
+    scarfyAnim.rec.y = 0;
+    scarfyAnim.pos.x = windowWidth / 2 - scarfyAnim.rec.width / 2;
+    scarfyAnim.pos.y = windowHeight - scarfyAnim.rec.height;
+    scarfyAnim.frame = 0;
+    scarfyAnim.updateTime = 1.0 / 12.0;
+    scarfyAnim.runningTime = 0.0;
+
+    // object variables
+    Texture2D obstacle = LoadTexture("resources/Raylib_logo.png");
+    Rectangle obRec;
+    obRec.width = obstacle.width;
+    obRec.height = obstacle.height;
+    obRec.x = 0;
+    obRec.y = 0;
+    Vector2 obPos;
+    obPos.x = windowWidth - obRec.width;
+    obPos.y = windowHeight - obRec.height;
+
+    // movement and collison varables
+    int obVel{-100};
+    int velocity{0};
+    const int jumpHeight{500};
+    bool jumped{false};
+    int speed{200};
+    bool collision{};
 
     SetTargetFPS(60);
-    // loop where game logic takes place, only ends on window close
-    while (WindowShouldClose() == false)
+    while (!WindowShouldClose())
     {
+        const float deltaTime{GetFrameTime()};
+        Rectangle obstacleRec{
+            obPos.x,
+            obPos.y,
+            obRec.height,
+            obRec.width,
+        };
+        Rectangle scarfyRec{
+            scarfyAnim.pos.x,
+            scarfyAnim.pos.y,
+            scarfyAnim.rec.height,
+            scarfyAnim.rec.width};
+        if (CheckCollisionRecs(scarfyRec, obstacleRec))
+        {
+            collision = true;
+        }
+
+        if (IsKeyDown(KEY_D) && !jumped)
+        {
+            scarfyAnim.pos.x += speed * deltaTime;
+            scarfyAnim.rec.width = scarfy.width / 6;
+
+            scarfyAnim.runningTime += deltaTime;
+            if (scarfyAnim.runningTime >= scarfyAnim.updateTime)
+            {
+                scarfyAnim.runningTime = 0.0;
+                scarfyAnim.rec.x = scarfyAnim.frame * scarfyAnim.rec.width;
+                scarfyAnim.frame++;
+                if (scarfyAnim.frame > 5)
+                {
+                    scarfyAnim.frame = 0;
+                }
+            }
+        }
+        if (IsKeyReleased(KEY_D) && !jumped)
+        {
+            scarfyAnim.frame = 0;
+            scarfyAnim.rec.x = scarfyAnim.frame * scarfyAnim.rec.width;
+        }
+
+        if (IsKeyDown(KEY_A) && !jumped)
+        {
+            scarfyAnim.pos.x -= speed * deltaTime;
+            scarfyAnim.rec.width = -scarfy.width / 6;
+
+            scarfyAnim.runningTime += deltaTime;
+            if (scarfyAnim.runningTime >= scarfyAnim.updateTime)
+            {
+                scarfyAnim.runningTime = 0.0;
+                scarfyAnim.rec.x = scarfyAnim.frame * scarfyAnim.rec.width;
+                scarfyAnim.frame++;
+                if (scarfyAnim.frame > 5)
+                {
+                    scarfyAnim.frame = 0;
+                }
+            }
+        }
+        if (IsKeyReleased(KEY_A) && !jumped)
+        {
+            scarfyAnim.frame = 0;
+            scarfyAnim.rec.x = scarfyAnim.frame * scarfyAnim.rec.width;
+        }
         BeginDrawing();
+
+        if (scarfyAnim.pos.y >= windowHeight - scarfy.height)
+        {
+            velocity = 0;
+            jumped = false;
+        }
+        else
+        {
+            velocity += gravity * deltaTime;
+            jumped = true;
+        }
+        if (IsKeyPressed(KEY_SPACE) && !jumped)
+        {
+            velocity -= jumpHeight;
+        }
+
+        scarfyAnim.pos.y += velocity * deltaTime;
+        obPos.x += obVel * deltaTime;
         ClearBackground(WHITE);
-        DrawTexture(background, 0, 0, WHITE);                // add background image
-        DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE); // add charachter image
+        if (collision)
+        {
+            DrawTexture(background, 0, 0, WHITE);                               // add background image
+            DrawText("GAME OVER", windowWidth / 4, windowHeight / 2, 150, RED); // Say Game Over on screen
+        }
+        else
+        {
+            DrawTexture(background, 0, 0, WHITE);                          // add background image
+            DrawTextureRec(scarfy, scarfyAnim.rec, scarfyAnim.pos, WHITE); // DrawRectangle(windowWidth/2,posY,width,height,GREEN);
+            DrawTextureRec(obstacle, obRec, obPos, WHITE);
+        }
         EndDrawing();
     }
-    UnloadTexture(background); // unload texture from memory
     UnloadTexture(scarfy);
+    UnloadTexture(obstacle);
     CloseWindow();
-    return 0;
 }
